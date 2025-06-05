03be37fa3e5205efda0f91593c42cb63ac0d7cbf
Jp2op <jpwork8698@gmail.com>
2025-06-05 21:13:21 +0530

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,113 +1,73 @@
 provider "aws" {
-  region = "us-east-1"
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-    }
-  }
+  region = "us-west-2"  # Replace with your region
 }
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.2"
+  version = "5.1.0"
 
-  name = "flask-eks-vpc"
+  name = "todo-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b"]
+  azs             = ["us-west-2a", "us-west-2b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
 
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "20.8.4"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.0.0"
 
-  cluster_name    = "flask-todo-cluster"
+  cluster_name    = "todo-cluster"
   cluster_version = "1.29"
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
 
   enable_irsa = true
 
-  cluster_endpoint_public_access       = true
-  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
-  cluster_endpoint_private_access      = false
+  eks_managed_node_group_defaults = {
+    instance_types = ["t3.medium"]
+  }
 
   eks_managed_node_groups = {
     default = {
-      desired_size   = 2
-      min_size       = 1
-      max_size       = 3
+      desired_size = 2
+      max_size     = 3
+      min_size     = 1
+
       instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
     }
   }
 
-  map_users = [
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+module "eks_aws_auth" {
+  source  = "terraform-aws-modules/eks/aws//modules/aws-auth"
+  version = "20.0.0"
+
+  depends_on = [module.eks]
+
+  manage_aws_auth_configmap = true
+  cluster_name              = module.eks.cluster_name
+
+  users = [
     {
       userarn  = "arn:aws:iam::203918885394:user/prefect-worker"
-      username = "github-actions-admin"
+      username = "prefect-worker"
       groups   = ["system:masters"]
     }
   ]
-
-  map_roles = []
-}
-
-resource "helm_release" "datadog" {
-  name       = "datadog"
-  repository = "https://helm.datadoghq.com"
-  chart      = "datadog"
-  version    = "3.53.1"
-
-  namespace        = "datadog"
-  create_namespace = true
-
-  set {
-    name  = "datadog.apiKey"
-    value = var.datadog_api_key
-  }
-
-  set {
-    name  = "datadog.site"
-    value = "datadoghq.com"
-  }
-
-  set {
-    name  = "datadog.logs.enabled"
-    value = "true"
-  }
-
-  set {
-    name  = "datadog.apm.enabled"
-    value = "true"
-  }
-
-  set {
-    name  = "datadog.processAgent.enabled"
-    value = "true"
-  }
-
-  set {
-    name  = "agents.containerLogs.enabled"
-    value = "true"
-  }
-
-  set {
-    name  = "datadog.env"
-    value = "production"
-  }
-
-  depends_on = [module.eks]
 }
