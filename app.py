7a25a5f73ce484329a41dc4d6128b0080db3ce39
Jp2op <jpwork8698@gmail.com>
2025-06-06 03:18:45 +0530

    Fix: Port conflict issuel

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def test_datadog_trace():
         return "Datadog trace captured."
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=80)
--- terraform/main.tf
+++ terraform/main.tf
@@ -20,6 +20,14 @@ module "vpc" {
     Name = "eks-vpc"
   }
 }
+resource "aws_security_group_rule" "allow_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.eks.node_security_group_id
+}
 #EKs module
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
