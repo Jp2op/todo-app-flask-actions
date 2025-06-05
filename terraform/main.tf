3c26174bc28bbc44c2871c98cd992dad77638e8d
Jp2op <jpwork8698@gmail.com>
2025-06-05 23:22:50 +0530

    Fix: remove unsupported map_users block

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,79 +1,49 @@
 provider "aws" {
-  region = "us-east-1" # Replace with your desired AWS region
+  region = var.region
 }
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.0"
+  version = "5.1.1"
 
-  name = "todo-vpc"
+  name = "eks-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+  azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
 
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
+    Name = "eks-vpc"
   }
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.0.0"
+  version = "19.15.3"
 
-  cluster_name    = "todo-cluster"
+  cluster_name    = var.cluster_name
   cluster_version = "1.29"
   subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
   enable_irsa = true
 
-  eks_managed_node_group_defaults = {
-    instance_types = ["t3.medium"]
-  }
-
   eks_managed_node_groups = {
-    default = {
-      desired_size = 2
-      max_size     = 3
-      min_size     = 1
+    default_node_group = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
 
       instance_types = ["t3.medium"]
-      capacity_type  = "ON_DEMAND"
     }
   }
-
-  # Add your IAM user mappings here
-  map_users = [
-    {
-      userarn  = "arn:aws:iam::203918885394:user/prefect-worker"
-      username = "prefect-worker"
-      groups   = ["system:masters"]
-    }
-  ]
 
   tags = {
     Environment = "dev"
     Terraform   = "true"
   }
 }
-
-# The module "eks_aws_auth" should be removed.
-# module "eks_aws_auth" {
-#   source = "terraform-aws-modules/eks/aws//modules/aws-auth"
-# 
-#   cluster_name = module.eks.cluster_name # This was causing the error
-# 
-#   aws_auth_users = [ # This structure should be used with map_users in the main EKS module
-#     {
-#       userarn  = "arn:aws:iam::203918885394:user/prefect-worker"
-#       username = "prefect-worker"
-#       groups   = ["system:masters"]
-#     }
-#   ]
-# }
